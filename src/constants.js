49c9a5ae44edeb57d05c4ccfa1ed39612270ccb2
Abhilov23 <abhilovgupta01@gmail.com>
2024-10-03 00:35:31 +0530

    update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,22 +2,20 @@ export const scaleFactor = 4;
 
 export const dialogueData = {
   pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    I've made a couple of games in that language. I also like Rust and Python. Anyway regardless of the language, I just like programming and cybersecurity.
+    Here is my <a href="https://github.com/abhilov23" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical computer-science degree but I think it gave me a good foundation.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
+  I also write articles on medium. Go sub to <a href="https://medium.com/@abhilov" target="_blank">my Blogs</a>! (If you like the content)`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+   <a href="https://www.youtube.com/@FarahHawa" target="_blank">Farah_Hawaa</a>, <a href="https://www.youtube.com/@BlackHatOfficialYT" target="_blank">Blackhat</a>,
+  <a href="https://www.youtube.com/@InsiderPhD" target="_blank">InsiderPHD</a> and <a href="https://www.youtube.com/@NahamSec" target="_blank">Nahamsec</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  resume: `This is my desk and on it is my resume. <a href="" target="_blank">Check it out?</a>
+  Contact me at abhilov01@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. You can also take a look at my <a href="https://jeweled-lathe-d5e.notion.site/Bugs-detailed-25ae98f3d3b648bba4e1ab155e6760cb?pvs=4" target="_blank">cybersecurity</a> and <a href="https://jeweled-lathe-d5e.notion.site/Development-notes-ecd9eacd30554114850f552cf4069233?pvs=4" target="_blank">Programming</a> notes.
+   Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Abhilov's portfolio, just close the tab.`,
 };
